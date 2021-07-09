8bafc93051a709c8e0ee283ebfabde6c9cba6cd8
Lamoz9ra <43520922+Lamoz9ra@users.noreply.github.com>
2021-07-09 23:47:31 +0300

    Add_new_main

--- SQL_HOMEWORK/Lesson_4 (MongoDB)/agg.js
+++
@@ -1,6 +0,0 @@
-# - подсчитайте число элементов в созданной коллекции
-db.tags.find().count()
-# - подсчитайте число фильмов с конкретным тегом - `woman`
-db.tags.find({name:"woman"}).count()
-# - используя группировку данных ($groupby) вывести top-3 самых распространённых тегов
-db.tags.aggregate([{$group:{_id: "$name", count:{$sum:1}}},{$sort:{count:-1}},{$limit: 3}])
